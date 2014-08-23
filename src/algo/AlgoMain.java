5e5cbb731e859bf89bc96562808c46aa50df7f29
Ruchika Marda <brewitbiz@gmail.com>
2014-08-23 15:20:20 -0400

    Added main intellij project file

---
+++ src/algo/AlgoMain.java
@@ -0,0 +1,12 @@
+package algo;
+
+/**
+ * Created by rmarda on 8/23/2014.
+ */
+public class AlgoMain {
+    public static void main(String args[]){
+
+        System.out.println("Hello World");
+    }
+
+}
